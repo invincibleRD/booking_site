efe95fa11c096dc92906e78d157ec5329b0194f1
Dipendra Kumar Shah <dipendrashah0789878@gmail.com>
2023-04-02 21:28:23 +0530

    finished form submission

--- pages/form.js
+++ pages/form.js
@@ -56,136 +56,111 @@ function Form() {
       ) : (
         <div>
           <div className={styles.body}>
-          <div className={styles.container}>
-            <div className={styles.title}>Registration</div>
-            <div className={styles.content}>
-              <form className={styles.form}>
-                <div className={styles.userDetails}>
-                  <div className={styles.input_box}>
-                    <span className={styles.details}>Full Name</span>
-                    <input type="text" placeholder="Enter your name" required />
+            <div className={styles.container}>
+              <div className={styles.title}>Registration</div>
+              <div className={styles.content}>
+                <form className={styles.form}>
+                  <div className={styles.userDetails}>
+                    <div className={styles.input_box}>
+                      <span className={styles.details}>Full Name</span>
+                      <input
+                        onChange={setFromData}
+                        type="text"
+                        name="full_name"
+                        placeholder="Enter your name"
+                        required
+                      />
+                    </div>
+                    <div className={styles.input_box}>
+                      <span className={styles.details}>Email</span>
+                      <input
+                        onChange={setFromData}
+                        name="email"
+                        type="email"
+                        placeholder="Enter your Email"
+                        required
+                      />
+                    </div>
+                    <div className={styles.input_box}>
+                      <span className={styles.details}>Phone Number</span>
+                      <input
+                        onChange={setFromData}
+                        name="phoneNo"
+                        type="number"
+                        placeholder="Enter your Number"
+                        required
+                      />
+                    </div>
+
+                    <div className={styles.input_box}>
+                      <span className={styles.details}>Education</span>
+                      <input
+                        onChange={setFromData}
+                        name="education"
+                        type="Text"
+                        placeholder="Enter Education"
+                        required
+                      />
+                    </div>
+                    <div
+                      className={styles.input_box + " " + styles.datetimeSize}
+                    >
+                      <span className={styles.details}>Session time</span>
+                      <input
+                        onChange={setFromData}
+                        name="slot_date_time"
+                        type="datetime-local"
+                        placeholder="Choose slote time"
+                        required
+                      />
+                    </div>
+                    <div className={styles.input_box}>
+                      <label className={styles.select}>Gender:</label>{" "}
+                      <select
+                        onChange={setFromData}
+                        name="gender"
+                        className={styles.select1}
+                        id="gender"
+                      >
+                        <option value="Male">Male </option>
+                        <option value="Female">Female</option>
+                        <option value="Others">Others</option>
+                      </select>
+                    </div>
+                    {/* <div className={styles.input_box + " " + styles.selectDiv}>
+                      <label className={styles.select}>
+                        Preffered Language:
+                      </label>{" "}
+                      <select
+                        onChange={setFromData}
+                        className={styles.select1}
+                        name="language"
+                        id="language"
+                      >
+                        <option value="English">English</option>
+                        <option value="Hindi">hindi </option>
+                      </select>
+                    </div> */}
+                    
                   </div>
-                  <div className={styles.input_box}>
-                    <span className={styles.details}>Email</span>
-                    <input
-                      type="email"
-                      placeholder="Enter your Email"
-                      required
-                    />
-                  </div>
-                  <div className={styles.input_box}>
-                    <span className={styles.details}>Phone Number</span>
-                    <input
-                      type="number"
-                      placeholder="Enter your Number"
-                      required
-                    />
-                  </div>
-                  <div className={styles.input_box}>
-                    <span className={styles.details}>Education</span>
-                    <input type="Text" placeholder="Enter Education" required />
-                  </div>
-                  <div className={styles.input_box}>
-                    <label className={styles.select}>Gender:</label>{" "}
-                    <select className={styles.select1} name="gender" id="gender">
-                      <option value="Male">Male </option>
-                      <option value="Female">Female</option>
-                      <option value="Others">Others</option>
-                    </select>
-                  </div>
-                  <div className={styles.input_box + ' ' + styles.selectDiv}>
-                    <label className={styles.select}>Language:</label>{" "}
-                    <select  className={styles.select1} name="language" id="language">
-                      <option value="English">English</option>
-                      <option value="Hindi">hindi </option>
-                    </select>
-                  </div>
-                </div>
-                <div className={styles.description}>
+                  <div className={styles.description}>
                     <span className={styles.details}>Description:</span>
-                  <textarea className={styles.textArea} placeholder="Enter topics you want to discuss" name="description"></textarea>
-                </div>
-              </form>
-            </div>
-          </div>
-          </div>
-          <div className={styles.container}>
-            <div className={styles.title}>Enter Details for {teacher}</div>
-            <div className={styles.content}>
-              <form onSubmit={handleSubmit} className={styles.form}>
-                <div className={styles.userDetails}>
-                  <span className={styles.details}>Full Name</span>
-                  <input
-                    type="text"
-                    id="full_name"
-                    onChange={setFromData}
-                    name="full_name"
-                    required
-                    placeholder="Enter your name"
-                  ></input>
-                </div>
-                  <label htmlFor="full_name">Fullname: </label>
-                <br></br>  <label htmlFor="teacher">teacher: </label>
-                <input
-                  type="text"
-                  id="teacher"
-                  onChange={setFromData}
-                  name="teacher"
-                  required
-                  placeholder="enter teacher name"
-                ></input>
-                <br></br>  <label htmlFor="education">education: </label>
-                <input
-                  type="text"
-                  id="education"
-                  onChange={setFromData}
-                  name="education"
-                  required
-                  placeholder="latest education qualification"
-                ></input>
-                <br></br>  <label htmlFor="email">education: </label>
-                <input
-                  type="email"
-                  id="email"
-                  onChange={() => {
-                    setFromData;
-                    settoemail(event.target.value);
-                  }}
-                  name="email"
-                  required
-                  placeholder="enter email"
-                ></input>
-                <br></br>  <label htmlFor="language">language: </label>
-                <input
-                  type="text"
-                  id="language"
-                  onChange={setFromData}
-                  name="language"
-                  required
-                  placeholder="enter language"
-                ></input>
-                <br></br>  <label htmlFor="description">description: </label>
-                <input
-                  type="text"
-                  id="description"
-                  onChange={setFromData}
-                  name="description"
-                  required
-                  placeholder="enter description"
-                ></input>
-                <br></br> {" "}
-                <label htmlFor="slot_date_time">Slote date time: </label>
-                <input
-                  type="datetime-local"
-                  id="slot_date_time"
-                  onChange={setFromData}
-                  name="slot_date_time"
-                  required
-                  placeholder="enter slote date time"
-                ></input>
-                <br></br>
-                <button>Click To Book</button>
-              </form>
+                    <textarea
+                      className={styles.textArea}
+                      onChange={setFromData}
+                      placeholder="Enter topics you want to discuss"
+                      name="description"
+                    ></textarea>
+                  </div>
+                  <div className={styles.button}>
+                    <input
+                      onClick={handleSubmit}
+                      type="submit"
+                      value="Confirm Booking"
+                    />
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
